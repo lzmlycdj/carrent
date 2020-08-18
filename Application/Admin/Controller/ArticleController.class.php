3c5218def2f90c48da38a7489bea8e1fe8754877
lzm2013lzm <lzm2013lzm@gmail.com>
2020-08-18 14:06:27 +0800

    18完成文章添加功能

--- Application/Admin/Controller/ArticleController.class.php
+++ Application/Admin/Controller/ArticleController.class.php
@@ -21,11 +21,59 @@ class ArticleController extends Controller
 
     public function add()
     {
+        $article=D('article');
+        if(IS_POST)
+       {
+            $data['title']=I('title');
+            $data['rizu']=I('rizu');
+            $data['num']=I('num');
+            $data['atype']=I('atype');
+            $data['author']=I('author');
+            $data['cateid']=I('cateid');
+            $data['content']=I('content');
+            $data['keywords']=I('keywords');
+            $data['des']=I('des');
+            $data['rem']=I('rem');
+            $data['time']=time();
+            if($_FILES['pic']['tmp_name']!='')
+            {
+                $upload = new \Think\Upload();// 实例化上传类
+                $upload->maxSize   =     3145728 ;// 设置附件上传大小
+                $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+                $upload->savePath  =      './Public/Uploads/'; // 设置附件上传目录
+                $upload->rootPath='./';
+                // 上传文件 
+                $info=$upload->uploadOne($_FILES['pic']);
+                if(!$info) {// 上传错误提示错误信息
+                    $this->error($upload->getError());
+                 }else{// 上传成功  
+                     $data['pic']=$info['savepath'].$info['savename']; 
+                     }
+            }
+
+            if($article->create($data))
+            {
+                if($article->add())
+                {
+                    $this->success('新增文章成功！',U('lst'));
+                }else
+                {
+                    $this->error('新增失败！');
+                }
+
+            }else
+            {
+                $this->error($article->getError());
+            }
+
+            return;
+        }
         $cate=D('cate');
         $cates=$cate->catetree();
         $this->assign('cates',$cates);
         $this->display();
     }
+
     // 无极限分类
 
     public function edit()
