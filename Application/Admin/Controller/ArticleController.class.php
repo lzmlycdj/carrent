17bb1c7a88b58b39ecb34ae3ca21bb2f0409f161
lzm2013lzm <lzm2013lzm@gmail.com>
2020-08-18 14:29:47 +0800

    文章分页小功能

--- Application/Admin/Controller/ArticleController.class.php
+++ Application/Admin/Controller/ArticleController.class.php
@@ -9,6 +9,23 @@ class ArticleController extends Controller
     public function lst()
     {
        
+        $article=D('article');
+        $where=1;
+        if($kw=I('kw')){
+            $where.=' AND title LIKE "%'.$kw.'%"';
+        }
+        if($search=I('search-sort')){
+            $where.=' AND cateid ='.$search;
+        }
+        $count=$article->where($where)->count();// 查询满足要求的总记录数
+        $Page=new \Think\Page($count,3);// 实例化分页类 传入总记录数和每页显示的记录数(25)
+        $show= $Page->show();// 分页显示输出
+        $list = $article->where($where)->order('id desc')->limit($Page->firstRow.','.$Page->listRows)->select();
+        $this->assign('list',$list);// 赋值数据集
+        $this->assign('page',$show);// 赋值分页输出
+        $cate=D('cate');
+        $cates=$cate->catetree();
+        $this->assign('cates',$cates);
         $this->display();
         
     }
