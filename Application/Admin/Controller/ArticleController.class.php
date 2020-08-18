b5f9d43844ee6a96dafd581913bfa68230d96e2c
lzm2013lzm <lzm2013lzm@gmail.com>
2020-08-18 15:22:09 +0800

    文章修改页面试图关联模型使用

--- Application/Admin/Controller/ArticleController.class.php
+++ Application/Admin/Controller/ArticleController.class.php
@@ -9,7 +9,7 @@ class ArticleController extends Controller
     public function lst()
     {
        
-        $article=D('article');
+        $article=D('ArticleView');
         $where=1;
         if($kw=I('kw')){
             $where.=' AND title LIKE "%'.$kw.'%"';
@@ -93,11 +93,66 @@ class ArticleController extends Controller
 
     // 无极限分类
 
-    public function edit()
+    public function edit($id)
     {
+        $article=D('article');
+        if(IS_POST)
+       {
+            $data['title']=I('title');
+            $data['id']=I('id');
+            $data['rizu']=I('rizu');
+            $data['num']=I('num');
+            $data['atype']=I('atype');
+            $data['author']=I('author');
+            $data['cateid']=I('cateid');
+            $data['content']=I('content');
+            $data['keywords']=I('keywords');
+            $data['des']=I('des');
+            $data['rem']=I('rem');
+            $data['time']=time();
+            if($_FILES['pic']['tmp_name']!='')
+            {
+                $upload = new \Think\Upload();// 实例化上传类
+                $upload->maxSize   =     3145728 ;// 设置附件上传大小
+                $upload->exts      =     array('jpg', 'gif', 'png', 'jpeg');// 设置附件上传类型
+                $upload->savePath  =      './Public/Uploads/'; // 设置附件上传目录
+                $upload->rootPath='./';
+                // 上传文件 
+                $info=$upload->uploadOne($_FILES['pic']);
+                if(!$info) {// 上传错误提示错误信息
+                    $this->error($upload->getError());
+                 }else{// 上传成功  
+                     $data['pic']=$info['savepath'].$info['savename']; 
+                     }
+            }
 
+            if($article->create($data))
+            {
+                if($article->save())
+                {
+                    $this->success('修改文章成功！',U('lst'));
+                }else
+                {
+                    $this->error('修改文章失败！');
+                }
+
+            }else
+            {
+                $this->error($article->getError());
+            }
+
+            return;
+        }
+
+        $artres=$article->find($id);
+        $this->assign('artres',$artres);
+
+        $cate=D('cate');
+        $cates=$cate->catetree();
+        $this->assign('cates',$cates);
         $this->display();
     }
+
     // 单个删除
     public function del($id)
     {
