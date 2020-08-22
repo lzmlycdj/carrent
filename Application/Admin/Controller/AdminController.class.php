1bfd7626a12f288002842742862ceda97a36007c
lzm2013lzm <lzm2013lzm@gmail.com>
2020-08-22 14:32:10 +0800

    34管理员分页显示

--- Application/Admin/Controller/AdminController.class.php
+++ Application/Admin/Controller/AdminController.class.php
@@ -9,10 +9,12 @@ class AdminController extends Controller
         if($kw=I('kw')){
             $where.=' AND username LIKE "%'.$kw.'%"';
         }
+        // 管理员列表显示
         $count=$admin->where($where)->count();// 查询满足要求的总记录数
         $Page=new \Think\Page($count,2);// 实例化分页类 传入总记录数和每页显示的记录数(25)
         $show= $Page->show();// 分页显示输出
-        $list = $admin->field('a.id,a.username,b.rolename')->alias('a')->join('LEFT JOIN cs_role b ON a.roleid=b.id')->where($where)->limit($Page->firstRow.','.$Page->listRows)->select();
+        $list = $admin->field('a.id,a.username,b.rolename')->alias('a')->join('LEFT JOIN cs_role b ON a.roleid=b.id')->where($where)
+                ->limit($Page->firstRow.','.$Page->listRows)->select();
         $this->assign('list',$list);// 赋值数据集
         $this->assign('page',$show);// 赋值分页输出
 
